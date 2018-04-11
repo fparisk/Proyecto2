1e9f3290a045abbb9e5a3a0b27e25378e7ca0204
fparisk <fpariskikut@gmail.com>
2018-04-11 10:57:27 -0600

    changed y_coded and Y_decoded init

--- code_decode.m
+++ code_decode.m
@@ -1,15 +1,19 @@
 function x()
 
 yn = zeros(64,1);
-y_coded = 0;
-y_decoded = 0;
+y_coded = [];
+y_decoded = [];
 [y,Fs] = audioread('C:\Users\parisfe\Desktop\Maestria\adquisicion y proc datos\Proyecto2\muestra.wav');
 
 % codec
 for n = 64:64:length(y)
     yn = y(n-63:n); % take blocks of 64 samples   
     Y = rfft(yn,64); % code
-    y_coded = cat(2,y_coded,Y); % concatenate y_tmp to y_coded
+    if isempty(y_coded)
+        y_coded = Y;
+    else
+        y_coded = cat(2,y_coded,Y); % concatenate y_tmp to output
+    end
 end
 
 disp('length y_coded: ');
@@ -19,7 +23,11 @@ disp(length(y_coded));
 for i = 33:33:length(y_coded)
     yn_coded = y_coded(i-32:i); % take blocks of 64 samples
     y_temp = irfft(yn_coded,64); % decode
-    y_decoded = cat(2,y_decoded,y_temp); % concatenate y_tmp to output 
+    if isempty(y_decoded)
+        y_decoded = y_temp;
+    else
+        y_decoded = cat(2,y_decoded,y_temp); % concatenate y_tmp to output
+    end
 end
 
 disp('length y: ');
